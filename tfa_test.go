e7297023dddb935b3deb253e1b6aec32077fcc77
and07 <and_07@mail.ru>
2018-08-10 10:41:01 +0000

    Add new file

---
+++ tfa_test.go
@@ -0,0 +1,25 @@
+package tfa
+
+import (
+	"testing"
+)
+
+
+
+func checkError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+
+func TestQRCode(t *testing.T) {
+
+
+}
+
+
+func TestValidate(t *testing.T) {
+
+
+}
